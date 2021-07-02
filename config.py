a0f3465ec21aa66acb47dff19f86cf7f7e6c76f4
GodSamomen <71833103+GodSamomen@users.noreply.github.com>
2021-07-02 16:25:32 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,19 +1,45 @@
-token = "@BotFather'dan Aldığınız Token Buraya"
 
+token = "1729609735:AAE7L_u3jKCp3aAxdqqB6w7Numtqdl2RrlA"
+
+# You can write the text that the bot will send as a message when start is typed here.
+#/start yazıldığı zaman botun mesaj olarak atacağı metin buraya istediğiniz yazabilirsininz
 start_text = '''
-Merhaba :D 
-Kedileri ve köpekleri seviyorsanız ve belki onlar için deliriyorsanız bu bot tam size göre.
-Samomen Sunucusunda milyar tane kedi ve köpek fotoğrafı var :)). Merak mı ettiniz? 👉 /yardım
-Developer Iletişim: t.me/samomenx
+	Hello: D
+		If you love cats and dogs and maybe crazy about them, this boot is for you.
+  		There are billions of cat and dog photos :)). Curious? 👉 /help
+	Developer Contact: t.me/samomenx
+
+	Merhaba :D 
+		Kedileri ve köpekleri seviyorsanız ve belki onlar için deliriyorsanız bu bot tam size göre.
+ 		milyar tane kedi ve köpek fotoğrafı var :)). Merak mı ettiniz? 👉 /yardım
+	Developer Iletişim: t.me/samomenx
 '''
 
+# You can write the text that the bot will send as a message when help is typed here.
 help_text = '''
-Karmaşık Bir Şey Yok! 🌚
-Sohbette veya özel sohbetimize "kedi" veya "köpek" kelimesini yazın
-Developer Discord: Samomen#0666
+	Nothing Complicated! 🌚
+		Type the word "cat" or "dog" in chat or in our private chat
+	Developer Dispute: Samomen#0666
+
+	Karmaşık Bir Şey Yok! 🌚
+		Sohbette veya özel sohbetimize "kedi" veya "köpek" kelimesini yazın
+	Developer Discord: Samomen#0666
 '''
 
 cat_mas = [
+	"Here comes the cat.",
+	"look at this cat",
+	"In a past life, you were this cat...",
+	"Beautiful cat.",
+	"Vagrants, programmers etc. What unites them? Of course the love for cats!",
+	"Well, she smiled quickly! It's inappropriate to feel sorry for such a cat",
+	"Cute cat)",
+	"Psh... did you order a photo?",
+	"If you have seen this cat, the day is not wasted",
+	"What are you if this cat didn't laugh at you????",
+	"I'll show you a cat",
+	#you can add more here
+
 	"İşte kedi geliyor.",
 	"bu kediye bakın",
 	"Geçmiş hayatta, bu kediydin...",
@@ -25,11 +51,11 @@ cat_mas = [
 	"Bu kediyi gördüyseniz, gün boşa gitmiş sayılmaz",
 	"Bu kedi sana gülmediyse sen nesin?????",
 	"Sana bir kedi göstereceğim ",
-	#buraya daha fazla şey ekleyebilirisinz
+
+	#buraya daha fazlasını ekleyebilirsin
 ]
 dog_mas = [
-	"Ve işte köpek",
 	"HAAAUUUUUUVVVVV!",
-	"Güzel bir köpek."
-	#buraya daha fazla şey ekleyebilirisinz
+	#you can add more here
+	#buraya daha fazlasını ekleyebilirsin
 ]
